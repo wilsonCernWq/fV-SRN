7cc6f8ed0bccd57b581d8060a800517d6d8f100b
WU Qi <wilson.over.cloud@gmail.com>
2022-10-03 21:07:33 -0700

    update

--- compile-library-server.sh
+++ compile-library-server.sh
@@ -3,7 +3,7 @@ mkdir build
 cd build
 export CC=gcc-9
 export CXX=g++-9
-cmake -DTORCH_PATH=${CONDA_PREFIX}/lib/python3.8/site-packages/torch \
+/home/qadwu/Software/cmake-3.22.1-linux-x86_64/bin/cmake -DTORCH_PATH=${CONDA_PREFIX}/lib/python3.8/site-packages/torch \
       -DGLM_INCLUDE_DIR=/home/qadwu/Software/glm-0.9.9.8/glm \
       -DRENDERER_BUILD_GUI=OFF -DRENDERER_BUILD_TESTS=OFF -DRENDERER_BUILD_CLI=OFF -DRENDERER_BUILD_TESTS=OFF -DRENDERER_BUILD_OPENGL_SUPPORT=OFF ..
 make -j32 VERBOSE=true
